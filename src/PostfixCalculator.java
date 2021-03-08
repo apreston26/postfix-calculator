176863fcaef1ad294e6d6757ac23f9924cdc3627
apreston26 <austinpreston314@gmail.com>
2021-03-08 16:33:40 -0700

    Got the calculator to handle single operator expressions

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,13 +1,25 @@
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+
 
 public class PostfixCalculator {
 
-    private DoubleStack doubleStack;
-    private Map<String, Operator> operatorMap;
+    private final DoubleStack doubleStack;
+    private HashMap<String, Operator> operatorMap;
 
-    PostfixCalculator() {
-//        operatorMap.put("Addition", Addition);
+    public PostfixCalculator() {
+        operatorMap = new HashMap<>();
+        this.operatorMap.put("add", new Addition());
+        this.operatorMap.put("+", new Addition());
+        this.operatorMap.put("sub", new Addition());
+        this.operatorMap.put("-", new Addition());
+        this.operatorMap.put("mult", new Addition());
+        this.operatorMap.put("*", new Addition());
+        this.operatorMap.put("div", new Addition());
+        this.operatorMap.put("/", new Addition());
+        this.operatorMap.put("print", new Addition());
+        this.operatorMap.put("=", new Addition());
         doubleStack = new DoubleStack();
 
     }
@@ -17,10 +29,51 @@ public class PostfixCalculator {
     }
 
     public void evalOperator(String operator) {
+        List<Double> operands = new LinkedList<>();
+        Addition addition = new Addition();
+        Subtraction subtraction = new Subtraction();
+        Multiplication multiplication = new Multiplication();
+        Division division = new Division();
+        Print print = new Print();
+        operatorMap.get(operator);
+
+        while (!doubleStack.isEmpty()) {
+            double val = doubleStack.pop();
+            operands.add(val);
+        }
+        if (operator.equals("+") || operator.equals("add")) {
+            double sum = addition.eval(operands);
+            doubleStack.push(sum);
+            System.out.println(sum);
+        }
+
+        if (operator.equals("-") || operator.equals("sub")) {
+            double difference = subtraction.eval(operands);
+            doubleStack.push(difference);
+            System.out.println(difference);
+        }
+
+        if (operator.equals("*") || operator.equals("mult")) {
+            double product = multiplication.eval(operands);
+            doubleStack.push(product);
+            System.out.println(product);
+        }
+
+        if (operator.equals("/") || operator.equals("div")) {
+            double quotient = division.eval(operands);
+            doubleStack.push(quotient);
+            System.out.println(quotient);
+        }
+
+        if (operator.equals("=") || operator.equals("print")) {
+            double result = print.eval(operands);
+            doubleStack.push(result);
+            System.out.println(result);
+        }
 
     }
 
-    static class Addition implements  Operator {
+    static class Addition implements Operator {
 
         @Override
         public int numArgs() {
@@ -29,13 +82,11 @@ public class PostfixCalculator {
 
         @Override
         public double eval(List<Double> args) {
-            double product = 0;
-            
-            return product;
+            return args.get(0) + args.get(1);
         }
     }
 
-    static class Subtraction implements  Operator {
+    static class Subtraction implements Operator {
 
         @Override
         public int numArgs() {
@@ -44,13 +95,11 @@ public class PostfixCalculator {
 
         @Override
         public double eval(List<Double> args) {
-            double product = 0;
-
-            return product;
+            return args.get(0) - args.get(1);
         }
     }
 
-    static class Multiplication implements  Operator {
+    static class Multiplication implements Operator {
 
         @Override
         public int numArgs() {
@@ -59,13 +108,11 @@ public class PostfixCalculator {
 
         @Override
         public double eval(List<Double> args) {
-            double product = 0;
-
-            return product;
+            return args.get(0) * args.get(1);
         }
     }
 
-    static class Division implements  Operator {
+    static class Division implements Operator {
 
         @Override
         public int numArgs() {
@@ -74,13 +121,11 @@ public class PostfixCalculator {
 
         @Override
         public double eval(List<Double> args) {
-            double product = 0;
-
-            return product;
+            return args.get(0) / args.get(1);
         }
     }
 
-    static class Print implements  Operator {
+    static class Print implements Operator {
 
         @Override
         public int numArgs() {
@@ -89,9 +134,7 @@ public class PostfixCalculator {
 
         @Override
         public double eval(List<Double> args) {
-            double product = 0;
-
-            return product;
+            return  args.get(0);
         }
     }
 
